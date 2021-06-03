e2f2cbb082c40366673d57868fc29e2622d07bbc
yuenwinyun <geek@yuenwinyun.com>
2021-06-03 04:07:17 +0000

    refactor: refactor

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,27 @@
 FROM codercom/code-server:3.10.2
 
 USER coder
-ENV SHELL=/bin/bash
+ENV TERRAFORM_VERSION=0.15.4
+
+RUN sudo chown -R coder:coder /home/coder/.local
 
 COPY deploy-container/settings.json .local/share/code-server/User/settings.json
 COPY deploy-container/entrypoint.sh /usr/bin/deploy-container-entrypoint.sh
 COPY deploy-container/rclone-tasks.json /tmp/rclone-tasks.json
 COPY ["deploy-container/entrypoint.sh", "deploy-container/extensions", "deploy-container/.bash_aliases",  "./"]
 
-RUN sudo chown -R coder:coder /home/coder/.local
-RUN sudo mkdir project && sudo chown -R coder:coder /home/coder/project
+# Install vscode extensions
+RUN for extension in $(cat extensions); do code-server --install-extension $extension done
 
 # Install nvm, yarn and pnpm
 RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-RUN /bin/bash -c "source .nvm/nvm.sh && nvm install 16 && npm -g install yarn pnpm" 
+RUN /bin/bash -c "source .nvm/nvm.sh && nvm install 16 && npm -g install yarn pnpm"
 
 # Install rclone
 RUN sudo apt-get update && sudo apt-get install unzip -y
 RUN curl https://rclone.org/install.sh | sudo bash -
 
-# Install terraform and rclone
-ENV TERRAFORM_VERSION=0.15.4
-
+# Install terraform(debian) and rclone
 ADD --chown=coder:coder https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip terraform.zip
 RUN sudo unzip -q ./terraform.zip -d /usr/bin && \
     sudo rm terraform.zip
--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -7,15 +7,11 @@ PREFIX="deploy-code-server"
 mkdir -p $START_DIR
 
 config_git() {
+    local GIT_USERNAME="${GIT_USERNAME:-yuenwinyun}"
+    local GIT_EMAIL="${GIT_EMAIL:-'geek@yuenwinyun.com'}"
+
     git config --global user.name $GIT_USERNAME
     git config --global user.email $GIT_EMAIL
-}
-
-install_vscode_extensions() {
-    for extension in $(cat /home/coder/extensions); do
-        code-server --install-extension $extension
-    done
-    rm /home/coder/extensions
 }
 
 set_rclone_config() {
@@ -25,20 +21,19 @@ set_rclone_config() {
 }
 
 set_vscode_tasks() {
-    RCLONE_VSCODE_TASKS="${RCLONE_VSCODE_TASKS:-true}"
+    local RCLONE_VSCODE_TASKS="${RCLONE_VSCODE_TASKS:-true}"
 
     if [ $RCLONE_VSCODE_TASKS = "true" ]; then
         echo "[$PREFIX] Applying VS Code tasks for rclone"
         cp /tmp/rclone-tasks.json /home/coder/.local/share/code-server/User/tasks.json
-        code-server --install-extension actboy168.tasks &
     fi
 }
 
 execute_rclone_scripts() {
-    RCLONE_AUTO_PUSH="${RCLONE_AUTO_PUSH:-true}"
-    RCLONE_AUTO_PULL="${RCLONE_AUTO_PULL:-true}"
-    RCLONE_REMOTE_PATH=${RCLONE_REMOTE_NAME:-code-server-remote}:${RCLONE_DESTINATION:-code-server-files}
-    RCLONE_SOURCE_PATH=${RCLONE_SOURCE:-$START_DIR}
+    local RCLONE_AUTO_PUSH="${RCLONE_AUTO_PUSH:-true}"
+    local RCLONE_AUTO_PULL="${RCLONE_AUTO_PULL:-true}"
+    local RCLONE_REMOTE_PATH=${RCLONE_REMOTE_NAME:-code-server-remote}:${RCLONE_DESTINATION:-code-server-files}
+    local RCLONE_SOURCE_PATH=${RCLONE_SOURCE:-$START_DIR}
 
     echo "rclone sync $RCLONE_SOURCE_PATH $RCLONE_REMOTE_PATH $RCLONE_FLAGS --exclude **/node_modules/** -vv" >/home/coder/push_remote.sh
     echo "rclone sync $RCLONE_REMOTE_PATH $RCLONE_SOURCE_PATH $RCLONE_FLAGS --exclude **/node_modules/** -vv" >/home/coder/pull_remote.sh
@@ -68,10 +63,6 @@ fi
 echo "[$PREFIX] Config git"
 
 config_git
-
-echo "[$PREFIX] Installing vscode extensions"
-
-install_vscode_extensions &
 
 echo "[$PREFIX] Starting code-server..."
 # Now we can run code-server with the default entrypoint
